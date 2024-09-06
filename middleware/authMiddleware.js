554743adad02ec50b2b50452c5c2856c2acf1b28
Javed Akhtar <javedsir301@gmail.com>
2024-09-06 18:24:38 +0530

    Update Authentication method

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,27 +1,18 @@
-const basicAuth = require('express-basic-auth');
+const jwt = require('jsonwebtoken');
 
-module.exports = basicAuth({
-  users: { 'admin': 'password' }, 
-  challenge: true,
-  realm: 'My App'
-});
+module.exports = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
 
+  const tokenWithoutBearer = token.startsWith('Bearer ') ? token.slice(7, token.length) : token;
 
-// const jwt = require('jsonwebtoken');
-
-// module.exports = (req, res, next) => {
-//   const token = req.headers['authorization'];
-//   if (!token) {
-//     return res.status(401).json({ message: 'No token provided' });
-//   }
-
-//   const tokenWithoutBearer = token.startsWith('Bearer ') ? token.slice(7, token.length) : token;
-
-//   jwt.verify(tokenWithoutBearer, process.env.JWT_SECRET, (err, decoded) => {
-//     if (err) {
-//       return res.status(401).json({ message: 'Failed to authenticate token' });
-//     }
-//     req.userId = decoded.id;
-//     next();
-//   });
-// };
+  jwt.verify(tokenWithoutBearer, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Failed to authenticate token' });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
